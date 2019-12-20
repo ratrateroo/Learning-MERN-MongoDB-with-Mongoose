cf9ef3907e4a82a72678d7f4b91bfeeac796edaa
Mark Leo <ratrateroo@gmail.com>
2019-12-21 02:29:42 +0800

    connected to database and  created schema

--- util/location.js
+++ util/location.js
@@ -5,11 +5,11 @@ const HttpError = require('../models/http-error');
 const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
 
 async function getCoordsForAddress(address) {
-  return {
-    lat: 40.7484474,
-    lng: -73.9871516
-  };
-/*   const response = await axios.get(
+  // return {
+  //   lat: 40.7484474,
+  //   lng: -73.9871516
+  // };
+  const response = await axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
       address
     )}&key=${API_KEY}`
@@ -27,7 +27,7 @@ async function getCoordsForAddress(address) {
 
   const coordinates = data.results[0].geometry.location;
 
-  return coordinates; */
+  return coordinates;
 }
 
 module.exports = getCoordsForAddress;
